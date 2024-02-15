e24b2e01df3781e848d80fc4454fdcef75cb52e7
Bulaien <brianjones71@googlemail.com>
2024-02-15 20:29:26 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 
-const menu =  document.querySelector('.menu');
+
+const menu = document.querySelector('.menu');
+const overlay = document.querySelector('.overlay');
+overlay.addEventListener( 'click', () => {
+    overlay.style.height='0%'
+});
 const menuBtn = document.querySelector(".button")
+const menuLinks = menu.querySelectorAll('a');
 menuBtn.addEventListener('click', () => {
- menu.classList.toggle("active");
+    menu.classList.toggle("active");
 })
+
+menuLinks.forEach(link => link.addEventListener('click', ()=>{
+    menu.classList.remove('active')
+})) 
